4fded9d8968e82fdb3ed3263ba8f9b15f69433b4
Taofeekolamide <taofeekolamide55@gmail.com>
2025-01-07 11:02:46 +0100

    my-portfolio

---
+++ form.js
@@ -0,0 +1,39 @@
+        (function(){emailjs.init({publicKey: "30dqvUycTT0PEK7PJ",});})();
+
+        function sendEmail(event) {
+            event.preventDefault();
+
+            // Collect form data
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const phone = document.getElementById("phone").value;
+            const services = document.getElementById("services").value;
+            const message = document.getElementById("message").value;
+
+            var templateParams = {
+                name: name,
+                email: email,
+                phone: phone,
+                services: services,
+                message: message,
+            };
+
+            // Send email via EmailJS
+            emailjs.send('service_lnf5yli', 'template_o8xq6to', templateParams).then(
+            (response) => {
+                alert('SUCCESS!', response.status, response.text);
+                document.getElementById("contact-form").reset();
+
+            },
+            (error) => {
+                alert('FAILED...', error);
+            },
+        );
+    }
+
+    function showmenu() {
+        document.getElementById("menu-nav").style.display = "block"
+    }
+    function hidemenu() {
+        document.getElementById("menu-nav").style.display = "none"
+    }
